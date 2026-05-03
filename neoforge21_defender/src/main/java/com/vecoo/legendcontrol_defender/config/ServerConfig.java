da7a224e4816e70cc5def6247dd8db61c4b43080
Vecoo <vecoo@list.ru>
2026-05-04 04:23:11 +1100

    added config comments

--- neoforge21_defender/src/main/java/com/vecoo/legendcontrol_defender/config/ServerConfig.java
+++ neoforge21_defender/src/main/java/com/vecoo/legendcontrol_defender/config/ServerConfig.java
@@ -1,11 +1,15 @@
 package com.vecoo.legendcontrol_defender.config;
 
 import com.vecoo.extralib.shade.spongepowered.configurate.objectmapping.ConfigSerializable;
+import com.vecoo.extralib.shade.spongepowered.configurate.objectmapping.meta.Comment;
 import lombok.Getter;
 
 @Getter
 @ConfigSerializable
+@SuppressWarnings("FieldMayBeFinal")
 public class ServerConfig {
+    @Comment("Maximum number of players in the trust list. Set to 0 to disable.")
     private int trustLimit = 15;
+    @Comment("The number of seconds before the protection of a legendary pokemon is removed.")
     private int protectedTime = 300;
 }
